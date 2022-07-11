2618aea49be54f6d4b741cfd34bf9377d643e4b5
Alex Nelson <alnelson@progrexion.com>
2022-07-11 15:22:48 -0500

    removing non-local stuff

--- src/server/routes.go
+++ src/server/routes.go
@@ -5,7 +5,7 @@ package server
 func (s Server) routes(){
 	s.router.HandleFunc("/health-check", s.handleHealthCheck())
 	s.router.HandleFunc("/facebook", s.handleFacebook())
-	s.router.HandleFunc("/send-llat", s.handleSendLLAT())
+	// s.router.HandleFunc("/send-llat", s.handleSendLLAT())
 
 	// s.router.HandleFunc("/get-long-access-token", s.handleGetLongAccessToken())
 
