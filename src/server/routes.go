8f58443cfe67143a0fd29fb51a137db81be33862
Alex Nelson <alnelson@progrexion.com>
2022-07-11 15:15:18 -0500

    attempting llat stuff

--- src/server/routes.go
+++ src/server/routes.go
@@ -5,6 +5,8 @@ package server
 func (s Server) routes(){
 	s.router.HandleFunc("/health-check", s.handleHealthCheck())
 	s.router.HandleFunc("/facebook", s.handleFacebook())
+	s.router.HandleFunc("/send-llat", s.handleSendLLAT())
+
 	// s.router.HandleFunc("/get-long-access-token", s.handleGetLongAccessToken())
 
 }
